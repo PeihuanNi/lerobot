9093e5c47c980a34e360394c55b970de3f17f62e
nipeihuan <peihuanni@gmail.com>
2026-01-27 21:16:28 +0800

    update eval_libero.sh

--- eval_libero.sh
+++ eval_libero.sh
@@ -1,5 +1,9 @@
+TMPDIR="/home/nipeihuan/tmp"
+mkdir -p "$TMPDIR"
+export TMPDIR TMP TEMP
+
 TOKENIZERS_PARALLELISM=false MUJOCO_GL=egl PYOPENGL_PLATFORM=egl lerobot-eval \
-  --policy.path=/home/nipeihuan/lerobot/outputs/train/spatial/checkpoints/last/pretrained_model \
+  --policy.path=/home/nipeihuan/models/pi0_libero_finetuned \
   --policy.n_action_steps=10 \
   --env.type=libero \
   --env.task=libero_spatial \
